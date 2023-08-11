91bf8e80fc7c1b2cb5457b83808e32cbecac2b3b
mohammad sadra nafe <sadranafe7@gmail.com>
2023-08-11 18:43:04 +0330

    transaction structure added to transaction.jsx with destructuring the props and using them

--- src/components/expenseTracker/transaction.jsx
+++ src/components/expenseTracker/transaction.jsx
@@ -1,8 +1,21 @@
 
-const Transaction = () => {
+const Transaction = ({title , price , action}) => {
     return (
         <>
+            <div className = 'bg-neutral-100 flex flex-wrap justify-between items-center px-5 p-3 my-4 rounded-xl shadow-md shadow-gray-300'>
+                <div>
+                    <p>{title}</p>
+                </div>
 
+
+                <div>
+                    <p className =  'inline-block'>{price}</p>
+
+                    <button className = 'inline-block ml-3 px-3 p-1 text-red-600'>
+                        <i className = 'bx bx-trash text-lg'></i>
+                    </button>
+                </div>
+            </div>
         </>
     );
 };
