d4b3df6a84ddb7ba866d44e59f4c43e724f6c449
mohammad sadra nafe <sadranafe7@gmail.com>
2023-08-12 00:25:23 +0330

    add some spaces and removed some extra lines to have a clean code

--- src/components/expenseTracker/expenseTracker.jsx
+++ src/components/expenseTracker/expenseTracker.jsx
@@ -97,32 +97,32 @@ const ExpenseTracker = () => {
 
     return (
         <>
-            <div className="bg-neutral-200 select-none w-screen h-screen flex flex-wrap justify-center content-center items-center">
+            <div className = "bg-neutral-200 select-none w-screen h-screen flex flex-wrap justify-center content-center items-center">
 
-                <div className="w-4/12 relative text-white bg-sky-950 shadow-xl shadow-gray-400 rounded-xl p-3 mb-10 py-10 flex flex-wrap justify-center items-center">
-                    <h2 className="uppercase w-full text-center">current balance</h2>
-                    <p className="mt-4 text-xl">
+                <div className = "w-4/12 relative text-white bg-sky-950 shadow-xl shadow-gray-400 rounded-xl p-3 mb-10 py-10 flex flex-wrap justify-center items-center">
+                    <h2 className = "uppercase w-full text-center">current balance</h2>
+                    <p className = "mt-4 text-xl">
                         {
                             showBalance ? `$  ${Number(price).toLocaleString()}` :
                                 <>
-                                    <i className="bx bxl-flickr"></i>
-                                    <i className="bx bxl-flickr"></i>
-                                    <i className="bx bxl-flickr"></i>
+                                    <i className = "bx bxl-flickr"></i>
+                                    <i className = "bx bxl-flickr"></i>
+                                    <i className = "bx bxl-flickr"></i>
                                 </>
                         }
 
                     </p>
 
-                    <button onClick={balanceHandler} className={price.toString().length >= 12 ? "py-1 mt-2 px-3 absolute right-24 top-16" : "py-1 mt-2 px-3 absolute right-32 top-16"}>
-                        <i className={showBalance ? "bx bx-show-alt text-xl" : "bx bx-hide text-xl"}></i>
+                    <button onClick = {balanceHandler} className = {price.toString().length >= 12 ? "py-1 mt-2 px-3 absolute right-24 top-16" : "py-1 mt-2 px-3 absolute right-32 top-16"}>
+                        <i className = {showBalance ? "bx bx-show-alt text-xl" : "bx bx-hide text-xl"}></i>
                     </button>
 
-                    <div className="w-full px-5 mt-5">
-                        <div className="income_expense flex flex-wrap justify-between">
-                            <div className="income_wrapper">
-                                <div className="flex flex-wrap items-center content-center">
-                                    <i className="bx bx-up-arrow-circle text-2xl"></i>
-                                    <div className="ml-2">
+                    <div className = "w-full px-5 mt-5">
+                        <div className = "income_expense flex flex-wrap justify-between">
+                            <div className = "income_wrapper">
+                                <div className = "flex flex-wrap items-center content-center">
+                                    <i className = "bx bx-up-arrow-circle text-2xl"></i>
+                                    <div className = "ml-2">
                                         <p>incomes</p>
                                         <p>$ {Number(incomes).toLocaleString()}</p>
                                     </div>
@@ -130,10 +130,10 @@ const ExpenseTracker = () => {
                             </div>
 
 
-                            <div className="expense_wrapper">
-                                <div className="flex flex-wrap items-center content-center">
-                                    <i className="bx bx-down-arrow-circle text-2xl"></i>
-                                    <div className="ml-2">
+                            <div className = "expense_wrapper">
+                                <div className = "flex flex-wrap items-center content-center">
+                                    <i className = "bx bx-down-arrow-circle text-2xl"></i>
+                                    <div className = "ml-2">
                                         <p>expenses</p>
                                         <p>$ {Number(expenses) === 0 ? Number(expenses).toLocaleString() : Number(-expenses).toLocaleString()}</p>
                                     </div>
@@ -143,38 +143,42 @@ const ExpenseTracker = () => {
                     </div>
                 </div>
 
-                <div className="transaction_wrapper w-full text-center flex flex-wrap justify-center items-center">
-                    <div className="w-full mb-5">
-                        <button onClick={modalHandler} className="bg-yellow-400 p-2 rounded-lg hover:bg-yellow-500 transition-all">
-                            Add new
-                            <i className="bx bx-plus"></i>
-                        </button>
+                <div className = "transaction_wrapper w-full text-center flex flex-wrap justify-center items-center">
+                    <div className = "transaction_wrapper w-full text-center flex flex-wrap justify-center items-center">
+                        <div className = "w-full mb-5">
+                            <button onClick = {modalHandler} className = "bg-yellow-400 p-2 rounded-lg hover:bg-yellow-500 transition-all">
+                                Add new
+                                <i className = "bx bx-plus"></i>
+                            </button>
 
-                        <TransactionModal onModalHandler={modalHandler} onModalIsOpen={modalIsOpen} onTransactionHandler={transactionHandler} />
-                    </div>
-
-                    <div className="w-full flex-wrap flex justify-center items-center">
-                        <div className="w-6/12 flex flex-wrap justify-between items-center text-center">
-                            <p className="capitalize">
-                                <i className="bx bx-transfer-alt text-xl translate-y-1 mr-2"></i>
-                                transactions
-                            </p>
+                            <TransactionModal onModalHandler = {modalHandler} onModalIsOpen = {modalIsOpen} onTransactionHandler = {transactionHandler} />
                         </div>
 
-                        <FilteredTransaction selected={filteredView} onfilteredHandler={filteredTransactionHandler} />
-                    </div>
-                </div>
+                        <div className = "w-full flex-wrap flex justify-center items-center">
+                            <div className = "w-6/12 flex flex-wrap justify-between items-center text-center">
+                                <p className = "capitalize">
+                                    <i className = "bx bx-transfer-alt text-xl translate-y-1 mr-2"></i>
+                                    transactions
+                                </p>
+                            </div>
 
-                <div className='w-6/12 h-60 overflow-scroll'>
-                    {
-                        filteredTransactions.length !== 0 ?
-                            filteredTransactions.map((item, index) =>
-                                <div key={index}>
-                                    <Transaction title={item.title} price={item.price} action={item.action} onDelete={() => deleteBtnHandler(item)} />
-                                </div>
-                            )
-                            : <p className="py-2 mt-5 capitalize">no transaction found ❗</p>
-                    }
+                            <FilteredTransaction selected = {filteredView} onfilteredHandler = {filteredTransactionHandler} />
+                        </div>
+                    </div>
+
+                    <div className = 'w-6/12 h-60 overflow-scroll'>
+                        <div>
+                            {
+                                filteredTransactions.length !== 0 ?
+                                    filteredTransactions.map( (item, index) =>
+                                        <div key = {index}>
+                                            <Transaction title = {item.title} price = {item.price} action = {item.action} onDelete = { () => deleteBtnHandler(item) } />
+                                        </div>
+                                    )
+                                    : <p className = "py-2 mt-5 capitalize">no transaction found ❗</p>
+                            }
+                        </div>
+                    </div>
                 </div>
 
             </div>
--- src/components/expenseTracker/filteredTransaction.jsx
+++ src/components/expenseTracker/filteredTransaction.jsx
@@ -8,11 +8,11 @@ const FilteredTransaction = ({selected , onfilteredHandler}) => {
         <>
             <div>
                 <div>
-                    <i className="bx bx-filter-alt text-xl mr-3"> : </i>
-                    <select value = {selected} onChange = {dropDownHandler} name="dropdown" id="dropdown" className='appearance-none outline-none bg-transparent cursor-pointer'>
-                        <option value="view all">view all</option>
-                        <option value="deposit">deposit</option>
-                        <option value="withdraw">withdraw</option>
+                    <i className = "bx bx-filter-alt text-xl mr-3"> : </i>
+                    <select value = {selected} onChange = {dropDownHandler} name = "dropdown" id = "dropdown" className = 'appearance-none outline-none bg-transparent cursor-pointer'>
+                        <option value = "view all">view all</option>
+                        <option value = "deposit">deposit</option>
+                        <option value = "withdraw">withdraw</option>
                     </select>
                 </div>
             </div>
