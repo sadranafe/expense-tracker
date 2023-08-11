dd73c37f8a3e08a92eaf7937f1e7dd98eb45242c
mohammad sadra nafe <sadranafe7@gmail.com>
2023-08-11 18:44:20 +0330

    set action condition for price in transaction.jsx

--- src/components/expenseTracker/transaction.jsx
+++ src/components/expenseTracker/transaction.jsx
@@ -9,7 +9,7 @@ const Transaction = ({title , price , action}) => {
 
 
                 <div>
-                    <p className =  'inline-block'>{price}</p>
+                    <p className =  {action === "withdraw" ? 'inline-block text-red-600' : "inline-block text-green-500" }>{action === "withdraw" && price !== 0 ? `$ ${Number(-price).toLocaleString()}` : `$ ${Number(price).toLocaleString()}` }</p>
 
                     <button className = 'inline-block ml-3 px-3 p-1 text-red-600'>
                         <i className = 'bx bx-trash text-lg'></i>
